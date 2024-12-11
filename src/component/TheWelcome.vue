002be6d6980c8d37be09299345569e1bf20bb24f
bwlee-dix <137477179+bwlee-dix@users.noreply.github.com>
2024-12-11 11:06:44 +0900

    init

---
+++ src/component/TheWelcome.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>하이루!!</div>
+</template>
---
+++ src/view/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <h1>This is an Home page</h1>
+</template>
