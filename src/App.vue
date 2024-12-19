afe8d4e76903c8ee745d06b51b99e10513ff734f
bwlee-dix <137477179+bwlee-dix@users.noreply.github.com>
2024-12-19 15:25:25 +0900

    fix: delete unused comment

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,18 @@ import VideoModal from './component/VideoModal.vue'
 import CommonModal from './component/CommonModal.vue'
 import ScreenModal from './component/ScreenModal.vue'
 
-const modalVisible = ref(false) // 모달 상태
-const startY = ref(0) // 제스처 시작 Y좌표
-const currentY = ref(0) // 현재 Y좌표
-const isDragging = ref(false) // 드래깅 상태
-const threshold = 100 // 드래그 임계값
+const modalVisible = ref(false)
+const startY = ref(0)
+const currentY = ref(0)
+const isDragging = ref(false)
+const threshold = 100
 
 const handlePointerDown = (event: PointerEvent) => {
   console.log('Pointer Down:', event.clientY)
   if (!modalVisible.value && event.clientY < 50) {
-    // 화면 최상단에서 드래그 시작
     startY.value = event.clientY
     isDragging.value = true
   } else if (modalVisible.value) {
-    // 모달이 열려 있을 때 드래그 시작
     startY.value = event.clientY
     isDragging.value = true
   }
@@ -33,12 +31,10 @@ const handlePointerMove = (event: PointerEvent) => {
   console.log('Pointer Move:', deltaY)
 
   if (!modalVisible.value && deltaY > threshold) {
-    // 아래로 드래그하여 모달 열기
     modalVisible.value = true
     console.log('Modal Opened')
     resetDragState()
   } else if (modalVisible.value && -deltaY > threshold) {
-    // 위로 드래그하여 모달 닫기
     modalVisible.value = false
     console.log('Modal Closed')
     resetDragState()
